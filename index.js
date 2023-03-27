caa3a3d1eb7b29d48df389511776fa0fe93d326f
Coler Alberto <114001679+coleralberto@users.noreply.github.com>
2023-03-27 18:56:22 +0300

    uptdate uri

--- index.js
+++ index.js
@@ -6,13 +6,48 @@ const next = document.querySelector(".next");
 const links = document.querySelectorAll('a[href^="#"]');
 const btnUrg = document.querySelector(".urgente");
 const originalText = btnUrg.textContent;
-const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
-const btnCloseModal = document.querySelector(".close-modal");
-const btnBianca = document.querySelector("#Bianca");
+const btnCloseModal = document.querySelectorAll(".close-modal");
 const closeHamburger = document.querySelector("main");
-
+const options = document.querySelectorAll(".medic");
+const modals = document.querySelectorAll(".modal");
 let currentSlide = 0;
+
+options.forEach((option, index) => {
+  option.addEventListener("click", () => {
+    modals.forEach((modal) => {
+      modal.classList.add("hidden");
+    });
+
+    modals[index].classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
+});
+
+btnCloseModal.forEach((btnClose, index) => {
+  btnClose.addEventListener("click", function () {
+    modals[index].classList.add("hidden");
+    overlay.classList.add("hidden");
+  });
+});
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape") {
+    let currentModal = document.querySelector(".modal:not(.hidden)");
+    if (currentModal) {
+      currentModal.classList.add("hidden");
+      overlay.classList.add("hidden");
+    }
+  }
+});
+
+overlay.addEventListener("click", function () {
+  overlay.classList.add("hidden");
+ let currentModal = document.querySelector(".modal:not(.hidden)");
+  if (currentModal) {
+    currentModal.classList.add("hidden");
+  }
+});
 
 hamburger.addEventListener("click", () => {
   menuItems.classList.toggle("show");
@@ -32,25 +67,6 @@ btnUrg.addEventListener("mouseover", function () {
 });
 btnUrg.addEventListener("mouseout", function () {
   btnUrg.textContent = originalText;
-});
-
-const openModal = function () {
-  modal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
-};
-
-const closeModal = function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-};
-btnBianca.addEventListener("click", openModal);
-btnCloseModal.addEventListener("click", closeModal);
-overlay.addEventListener("click", closeModal);
-
-document.addEventListener("keydown", function (e) {
-  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
-    closeModal();
-  }
 });
 
 function showSlide() {
