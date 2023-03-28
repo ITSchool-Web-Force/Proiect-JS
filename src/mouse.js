981b6308d4a9e4eda8c8cd8c1d6f7dc2ef98fbde
Elena Tabacaru <120025210+elenatabacaru@users.noreply.github.com>
2023-03-28 15:44:12 +0300

    final

--- src/mouse.js
+++ src/mouse.js
@@ -2,7 +2,7 @@ window.addEventListener('load', (init) => {
     var pointer=document.getElementById('cursor');
     window.addEventListener('mousemove', mouseOver);
         function mouseOver(evt) {
-            cursor.style.left = (evt.clientX-32) + 'px';
-            cursor.style.top = (evt.clientY-32) + 'px';
+            cursor.style.left = (evt.clientX-5) + 'px';
+            cursor.style.top = (evt.clientY-5) + 'px';
         }
 })
