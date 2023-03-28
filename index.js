21097cae979398947450ffeac375802e94b4b6c5
Claudiu <nistor.claudiu92@gmail.com>
2023-03-28 11:28:19 +0300

    project js

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 window.addEventListener("load", function() {
-    var age = prompt("You need to specify your age");
+    if (!localStorage.getItem("agePromptShown")) {
+        var age = prompt("You need to be 18+ to enter this website");
 
-    if (age === null || age === ` ` || isNaN(age)) {
-        alert("You need to enter a valid number to enter this website");
-        window.location.href = `about:blank`;
-    } else if (age < 18) {
-        alert("Sorry, you need to be 18+ to enter this website");
-        window.location.href = `about:blank`;
-    } else {
+        if (age === null || age === ` ` || isNaN(age)) {
+            alert("You need to enter a valid number to enter this website");
+            window.location.href = `about:blank`;
+        } else if (age < 18) {
+            alert("Sorry, you need to be at least 18+ to enter this website");
+            window.location.href = `about:blank`;
+        } else {
+            localStorage.setItem("agePromptShown", true);
+        };
     }
-})
+
+    var audio = document.getElementById("myAudio");
+    audio.muted = true;
+    audio.autoplay = true;
+
+    audio.addEventListener("canplaythrough", function() {
+        audio.play();
+    });
+
+    document.addEventListener("keypress", function(event) {
+        if (event.key === "M" || event.key === "m") {
+            audio.muted = !audio.muted;
+        }
+    });
+});
